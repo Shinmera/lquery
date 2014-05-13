971a0ce632c6a149e2836b7619deed9a8123fee1
Shinmera <shinmera@tymoon.eu>
2014-05-13 11:06:25 +0200

    Rewrote functions to use plump, clss and array functions

--- lquery.lisp
+++ lquery.lisp
@@ -8,32 +8,9 @@
 
 (defvar *lquery-master-document* NIL)
 
-(defun load-file-quickly (pathname)
-  (with-open-file (stream pathname)
-    (let ((seq (make-array (file-length stream) :element-type 'character :fill-pointer t)))
-      (setf (fill-pointer seq) (read-sequence seq stream))
-      seq)))
-
-(defun load-page (file-or-string &key (type :XML) (builder (cxml-dom:make-dom-builder)))
-  "Load the given file or string into a HTML DOM.
-If type is :HTML, the file is treated as a full HTML document.
-If type is NIL or :XML, the file is treated as an XML document, which does not need to contain the HTML root nodes.
-Alternatively, the type can be a DTD specification that will be supplied to cxml:parse 's dtd keyword."
-  (flet ((parse (html)
-           (case type
-             (:HTML
-              (chtml:parse html builder))
-             ((NIL :XML)
-              (flet ((resolver (pubid sysid)
-                       (declare (ignore pubid sysid))
-                       (flexi-streams:make-in-memory-input-stream nil)))
-                (cxml:parse html builder :entity-resolver #'resolver)))
-             (T 
-              (cxml:parse html builder :dtd type)))))
-    (etypecase file-or-string
-      (pathname (with-open-file (html file-or-string :element-type '(unsigned-byte 8))
-                  (parse html)))
-      (string (parse file-or-string)))))
+(defun load-page (file-or-string)
+  "Load the given file or string into a HTML DOM."
+  (plump:parse file-or-string))
 
 (defun initialize (document)
   "Sets the *lquery-master-document* variable to the provided document."
@@ -50,22 +27,24 @@ All node functions are automatically created in the lquery-funcs package."
   (unless (stringp docstring)
     (setf body (append (list docstring) body))
     (setf docstring (format NIL "lQuery node function ~a" name)))
-  (let ((argslist (cons node-name
-                        (loop for arg in arguments
-                           unless (find arg '(&optional &key &allow-other-keys &rest))
-                           collect (if (listp arg) (first arg) arg))))
-        (funsymb (gensym "FUN")))
+  (let ((argslist (loop for arg in arguments
+                        unless (find arg '(&optional &key &allow-other-keys &rest))
+                          collect (if (listp arg) (first arg) arg)))
+        (funsymb (gensym "FUN"))
+        (i (gensym "I")))
         
     `(defun ,(intern (format NIL "NODEFUN-~a" name) :lquery-funcs) (,node-name ,@arguments)
        ,docstring
        (flet ((,funsymb ,argslist ,@body))
-         (if (listp ,node-name)
-             (alexandria:flatten 
-              (mapcar #'(lambda (,node-name) (,funsymb ,@argslist)) ,node-name))
-             (,funsymb ,@argslist))))))
+         (if (arrayp ,node-name)
+             (loop for ,i from 0 below (length ,node-name)
+                   do (setf (aref ,node-name ,i)
+                            (,funsym (aref ,node-name ,i) ,@argslist))
+                   finally (return ,node-name))
+             (,funsymb ,node-name ,@argslist))))))
 
 (defmacro define-node-list-function (name (list-name &rest arguments) &optional docstring &body body)
-  "Defines a new function that operates on the current node list instead of individual elements.
+  "Defines a new function that operates on the current node array instead of individual elements.
 All node list functions are automatically created in the lquery-funcs package."
   (unless (stringp docstring)
     (setf body (append (list docstring) body))
@@ -77,7 +56,7 @@ All node list functions are automatically created in the lquery-funcs package."
     `(defun ,(intern (format NIL "NODEFUN-~a" name) :lquery-funcs) (,list-name ,@arguments)
        ,docstring
        ,@declarations
-       (unless (listp ,list-name) (setf ,list-name (list ,list-name)))
+       (unless (arrayp ,list-name) (setf ,list-name (make-array 1 :initial-element ,list-name)))
        ,@body)))
 
 (defmacro $ (&body actions)
@@ -85,7 +64,7 @@ All node list functions are automatically created in the lquery-funcs package."
 
 Each argument is executed in sequence. The arguments are evaluated according to the defined argument-handlers. ~
 By default, the following cases are handled: 
-  * STRING    Translates to a CSS-select on the current elements.
+  * STRING    Translates to a CLSS:QUERY on the current elements.
   * LIST      What lists translate to is determined by their first element:
     * EVAL      The second value of the list is evaluated at compile-time and the result ~
                 is inlined as if it had been there as a standard argument to $.
@@ -102,7 +81,7 @@ By default, the following cases are handled:
 
 Symbols/Variables are handled at runtime (where they have a value) according to the defined ~
 symbol-handlers. By default, the following cases are handled at run time:
-  * STRING    Performs a CSS-select on the current elements.
+  * STRING    Performs a CLSS:QUERY on the current elements.
   * DOM:NODE  Replaces the current set of nodes with just this node.  
   * FUNCTION  Calls the given function with the current set of nodes as argument.
   * T         Any other value simply replaces the current list of nodes.
@@ -112,8 +91,8 @@ define-symbol-handler, respectively.")
   (%$ (reverse actions)))
 
 (defun %$ (actions)
-  (if (zerop (length actions))
-      `(list *lquery-master-document*)
+  (if (null actions)
+      `(make-array 1 :initial-element *lquery-master-document*)
       (let ((action (car actions))
             (rest (cdr actions)))
         (determine-argument action (%$ rest)))))
@@ -142,7 +121,7 @@ define-symbol-handler, respectively.")
   `(determine-symbol ,symbol ,nodes))
 
 (define-argument-handler string (string nodes)
-  `(css:query ,string ,nodes))
+  `(clss:select ',(clss:parse-selector string) ,nodes))
 
 (defgeneric determine-symbol (symbol nodes)
   (:documentation "Determines what to do with a given symbol at run-time (variable type)."))
@@ -157,11 +136,11 @@ define-symbol-handler, respectively.")
   variable)
 
 (define-symbol-handler string (selector nodes)
-  (css:query selector nodes))
+  (clss:select selector nodes))
 
-(define-symbol-handler dom:node (node nodes)
+(define-symbol-handler plump:node (node nodes)
   (declare (ignorable nodes))
-  (list node))
+  (make-array 1 :initial-element node))
 
 (define-symbol-handler function (function nodes)
   (funcall function nodes))
